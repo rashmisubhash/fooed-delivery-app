c6cbf15f405f693eeab451c0bbda190106345cb8
rashmi1120 <rashmi.bsubash@gmail.com>
2022-10-29 18:44:29 +0530

    init

--- src/App.js
+++ src/App.js
@@ -5,25 +5,21 @@ import LandingPage from "./pages/LandingPage";
 import RestaurentTemplate from "./pages/RestaurentMenu/RestaurentTemplate";
 
 // import { LastLocationProvider } from "react-router-last-location";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Switch, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <>
       <Nav />
-      <Router
-        getUserConfirmation={() => {
-          /* Empty callback to block the default browser prompt */
-        }}
-      >
-        {/* <LastLocationProvider> */}
-        {/* //  <LandingPage /> */}
-        <Routes>
-          <Route exact path="/" element={<LandingPage />}></Route>
-          <Route exact path="/order" element={<RestaurentTemplate />}></Route>
-        </Routes>
-        {/* </LastLocationProvider> */}
-      </Router>
+      {/* <BrowserRouter> */}
+      {/* <LastLocationProvider> */}
+      {/* //  <LandingPage /> */}
+      <Routes>
+        <Route exact path="/" element={<LandingPage />}></Route>
+        <Route exact path="/order" element={<RestaurentTemplate />}></Route>
+      </Routes>
+      {/* </LastLocationProvider> */}
+      {/* </BrowserRouter> */}
     </>
   );
 }
